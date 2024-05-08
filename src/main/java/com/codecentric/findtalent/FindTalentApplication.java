5db6b483a6fb49c6ca237d9cbe331b5c223db760
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-05-08 21:45:20 +0200

    initial commit

---
+++ src/main/java/com/codecentric/findtalent/FindTalentApplication.java
@@ -0,0 +1,13 @@
+package com.codecentric.findtalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTalentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindTalentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecentric/findtalent/FindTalentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecentric.findtalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindTalentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
