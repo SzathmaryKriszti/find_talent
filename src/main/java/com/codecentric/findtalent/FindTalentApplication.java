4aac1149f31ac774e6586ec38585210acbf15fcc
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-05-09 11:24:51 +0200

    add function to get organisation, members, repos and languages

--- src/main/java/com/codecentric/findtalent/FindTalentApplication.java
+++ src/main/java/com/codecentric/findtalent/FindTalentApplication.java
@@ -1,12 +1,23 @@
 package com.codecentric.findtalent;
 
-import com.jcabi.github.*;
+import org.kohsuke.github.GHOrganization;
+
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class FindTalentApplication {
@@ -21,21 +32,42 @@ public class FindTalentApplication {
         SmartInitializingSingleton singleton = new SmartInitializingSingleton() {
             @Override
             public void afterSingletonsInstantiated() {
-                final Github github = new RtGithub();
-                Organization org = github.organizations().get("codecentric");
+                GitHub github = null;
 
-                for (User user : org.publicMembers().iterate()) {
-                    try {
-                        System.out.println(user.login());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                try {
+                    github = GitHub.connect();
+                    //        GitHub github = GitHub.connectAnonymously();
+
+
+                    GHOrganization org = github.getOrganization("codecentric");
+                    List<GHUser> members = org.getMembers();
+
+
+                    for (GHUser member : members) {
+                        Map<String, GHRepository> memberRepos = member.getRepositories();
+                        for (GHRepository repository : memberRepos.values()){
+                           String language = repository.getLanguage();
+                            System.out.println(language);
+                        }
                     }
+
+
+
+
+
+
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
+
 
             }
         };
         return singleton;
     }
+
+
 }
 
 
