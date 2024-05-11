eb994823ac49f63ad11ae1d847fa88c9634dca08
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-05-11 09:41:13 +0200

    fix search function - returns unique members with all their repos

--- src/main/java/com/codecentric/findtalent/service/SearchService.java
+++ src/main/java/com/codecentric/findtalent/service/SearchService.java
@@ -3,14 +3,17 @@ package com.codecentric.findtalent.service;
 import com.codecentric.findtalent.domain.Member;
 import com.codecentric.findtalent.domain.Repo;
 import com.codecentric.findtalent.dto.outgoing.MemberDetails;
+import com.codecentric.findtalent.dto.outgoing.MemberDetailsItem;
 import com.codecentric.findtalent.repository.MemberRepository;
 import com.codecentric.findtalent.repository.RepoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -28,16 +31,19 @@ public class SearchService {
 
     public MemberDetails searchMemberByLanguage(String language) {
         List<Member> membersByLanguage = new ArrayList<>();
-        List<Repo> repos = repoRepository.findRepoByLanguage(language);
-        if (!repos.isEmpty()) {
-            for (Repo repo : repos) {
-                Member member = repo.getMember();
-                if (!membersByLanguage.contains(member)) {
-                    membersByLanguage.add(member);
-                }
-            }
+        List<Repo> repos = repoRepository.findReposByLanguage(language);
+
+        for (Repo repo : repos) {
+           Member member = repo.getMember();
+           membersByLanguage.add(member);
         }
-        return new MemberDetails(membersByLanguage);
+
+        Set<Member> uniqueMembers = new HashSet<>(membersByLanguage);
+        membersByLanguage.clear();
+        membersByLanguage.addAll(uniqueMembers);
+
+
+        return new MemberDetails(membersByLanguage.stream().map(MemberDetailsItem::new).collect(Collectors.toList()));
     }
 
 
