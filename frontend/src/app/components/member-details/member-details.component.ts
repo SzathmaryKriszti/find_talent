f097e20acf4fea63b84db9df1354b58afe6a07bd
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-05-13 14:57:01 +0200

    -refactor back button

--- frontend/src/app/components/member-details/member-details.component.ts
+++ frontend/src/app/components/member-details/member-details.component.ts
@@ -2,39 +2,34 @@ import {Component, OnInit} from '@angular/core';
 import {SearchService} from "../../services/search.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {MemberDetailsItemModel} from "../../models/member-details-item.model";
-import {Location} from "@angular/common";
 
 @Component({
-  selector: 'app-member-details',
-  templateUrl: './member-details.component.html',
-  styleUrls: ['./member-details.component.css']
+    selector: 'app-member-details',
+    templateUrl: './member-details.component.html',
+    styleUrls: ['./member-details.component.css']
 })
 export class MemberDetailsComponent implements OnInit {
 
-  id: any = 0;
-  memberDetails! : MemberDetailsItemModel;
+    id: any = 0;
+    memberDetails!: MemberDetailsItemModel;
 
-  constructor(private searchService: SearchService,
-              private activatedRoute: ActivatedRoute,
-              private location : Location,
-              private router: Router) {
-    this.activatedRoute.paramMap.subscribe(paramResponse => {
-      this.id = paramResponse.get('id');
-    })
-  }
+    constructor(private searchService: SearchService,
+                private activatedRoute: ActivatedRoute,
+                private router: Router) {
+        this.activatedRoute.paramMap.subscribe(paramResponse => {
+            this.id = paramResponse.get('id');
+        })
+    }
 
-  ngOnInit(): void {
-    this.loadMemberDetails(this.id);
-  }
+    ngOnInit(): void {
+        this.loadMemberDetails(this.id);
+    }
 
-  loadMemberDetails(id: number) {
-    this.searchService.loadMemberDetails(id).subscribe({
-      next: value => this.memberDetails = this.memberDetails = value,
-      error: err => console.error(err)
-    });
-  }
+    loadMemberDetails(id: number) {
+        this.searchService.loadMemberDetails(id).subscribe({
+            next: value => this.memberDetails = this.memberDetails = value,
+            error: err => console.error(err)
+        });
+    }
 
-  back() {
-    this.location.back();
-  }
 }
