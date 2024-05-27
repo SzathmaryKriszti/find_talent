41dd36e63eef47cb0dcfbd27910d358a06974432
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-05-27 11:20:52 +0200

    add error message, if backend gives an empty value by search method

--- frontend/src/app/components/search-member/search-member.component.ts
+++ frontend/src/app/components/search-member/search-member.component.ts
@@ -5,47 +5,53 @@ import {MemberDetailsItemModel} from "../../models/member-details-item.model";
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-    selector: 'app-search-member',
-    templateUrl: './search-member.component.html',
-    styleUrls: ['./search-member.component.css']
+  selector: 'app-search-member',
+  templateUrl: './search-member.component.html',
+  styleUrls: ['./search-member.component.css']
 })
 export class SearchMemberComponent implements OnInit {
 
-    members: Array<MemberDetailsItemModel> = [];
-    searchForm!: FormGroup
+  members: Array<MemberDetailsItemModel> = [];
+  searchForm!: FormGroup
+  errorMessage: Boolean = false;
 
-    constructor(private searchService: SearchService,
-                private formBuilder: FormBuilder,
-                private router: Router,
-                private activatedRoute: ActivatedRoute) {
-        this.searchForm = formBuilder.group({
-            language: ['', Validators.required]
-        })
-        this.activatedRoute.queryParams.subscribe(
-            params => {
-                this.searchForm.controls['language'].setValue(params['language'])
-            }
-        );
+  constructor(private searchService: SearchService,
+              private formBuilder: FormBuilder,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    this.searchForm = formBuilder.group({
+      language: ['', Validators.required]
+    })
+    this.activatedRoute.queryParams.subscribe(
+      params => {
+        this.searchForm.controls['language'].setValue(params['language'])
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.search();
+  }
+
+
+  search() {
+    if (this.searchForm.get('language')?.value) {
+      this.searchService.search(this.searchForm.get('language')?.value).subscribe({
+        next: (value) => {
+          if (value.length === 0) {
+            this.errorMessage = true
+          } else {
+            this.errorMessage = false
+            this.members = this.members = value
+            this.router.navigate([''], {queryParams: {language: this.searchForm.get('language')?.value}})
+          }
+        },
+        error: err => console.error(err)
+      });
     }
+  }
 
-    ngOnInit(): void {
-            this.search();
-    }
-
-
-    search() {
-        if(this.searchForm.get('language')?.value) {
-            this.searchService.search(this.searchForm.get('language')?.value).subscribe({
-                next: (value) => {
-                    this.members = this.members = value
-                    this.router.navigate([''], {queryParams: {language: this.searchForm.get('language')?.value}})
-                },
-                error: err => console.error(err)
-            });
-        }
-    }
-
-    loadRepos(id: number) {
-        this.router.navigate(["/member-details/" + id], {queryParams: {language: this.searchForm.get('language')?.value}});
-    }
+  loadRepos(id: number) {
+    this.router.navigate(["/member-details/" + id], {queryParams: {language: this.searchForm.get('language')?.value}});
+  }
 }
