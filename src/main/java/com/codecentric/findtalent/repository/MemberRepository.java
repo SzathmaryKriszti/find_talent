fc7be7dd368a04b6e2c210966dddc4da63e6574c
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-06-13 09:51:14 +0200

    change search logic in searchMemberByLanguage method

--- src/main/java/com/codecentric/findtalent/repository/MemberRepository.java
+++ src/main/java/com/codecentric/findtalent/repository/MemberRepository.java
@@ -2,8 +2,15 @@ package com.codecentric.findtalent.repository;
 
 import com.codecentric.findtalent.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>{
+
+
+    @Query("SELECT DISTINCT m FROM Member m JOIN Repo r ON m.id = r.member.id WHERE r.language = :language")
+    List<Member> findDistinctMembersByRepositoryLanguage(String language);
 }
--- src/main/java/com/codecentric/findtalent/service/SearchService.java
+++ src/main/java/com/codecentric/findtalent/service/SearchService.java
@@ -1,15 +1,14 @@
 package com.codecentric.findtalent.service;
 
 import com.codecentric.findtalent.domain.Member;
-import com.codecentric.findtalent.domain.Repo;
 import com.codecentric.findtalent.dto.outgoing.MemberDetailsItem;
 import com.codecentric.findtalent.repository.MemberRepository;
-import com.codecentric.findtalent.repository.RepoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,28 +16,15 @@ import java.util.stream.Collectors;
 public class SearchService {
 
     private final MemberRepository memberRepository;
-    private final RepoRepository repoRepository;
 
     @Autowired
-    public SearchService(MemberRepository memberRepository, RepoRepository repoRepository) {
+    public SearchService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
-        this.repoRepository = repoRepository;
     }
 
 
     public List<MemberDetailsItem> searchMemberByLanguage(String language) {
-        List<Member> membersByLanguage = new ArrayList<>();
-        List<Repo> repos = repoRepository.findReposByLanguage(language);
-
-        for (Repo repo : repos) {
-            Member member = repo.getMember();
-            membersByLanguage.add(member);
-        }
-
-        Set<Member> uniqueMembers = new LinkedHashSet<>(membersByLanguage);
-        membersByLanguage.clear();
-        membersByLanguage.addAll(uniqueMembers);
-
+        List<Member> membersByLanguage = memberRepository.findDistinctMembersByRepositoryLanguage(language);
 
         return membersByLanguage.stream().map(MemberDetailsItem::new).collect(Collectors.toList());
     }
